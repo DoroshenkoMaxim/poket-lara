3114631eab889647949c5d8da0e9c2da3f7e3486
DoroshenkoMaxim <36296940+DoroshenkoMaxim@users.noreply.github.com>
2025-06-28 17:54:42 +0300

    asd

--- resources/views/signals/index.blade.php
+++ resources/views/signals/index.blade.php
@@ -18,14 +18,14 @@
                     <!-- Фильтры -->
                     <div class="row mb-4">
                         <div class="col-md-6">
-                            <button type="button" class="btn btn-primary btn-lg w-100 filter-btn" 
+                            <button type="button" class="btn btn-outline-primary btn-sm w-100 filter-btn" 
                                     data-bs-toggle="modal" data-bs-target="#currencyModal" id="currencyBtn">
                                 <i class="fas fa-coins"></i>
                                 <span class="filter-title">Валюты</span>
                             </button>
                         </div>
                         <div class="col-md-6">
-                            <button type="button" class="btn btn-info btn-lg w-100 filter-btn" 
+                            <button type="button" class="btn btn-outline-info btn-sm w-100 filter-btn" 
                                     data-bs-toggle="modal" data-bs-target="#timeframeModal" id="timeframeBtn">
                                 <i class="fas fa-clock"></i>
                                 <span class="filter-title">Таймфреймы</span>
